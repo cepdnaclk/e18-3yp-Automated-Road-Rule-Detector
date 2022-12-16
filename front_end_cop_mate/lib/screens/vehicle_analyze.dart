e3fe429890f494d24ec4ec1132ef7693d56f52b9
SakuniJayasinghe <s.nimnadi@gmail.com>
2022-12-16 23:01:13 +0530

    added vehicle analyze plot

--- front_end_cop_mate/lib/screens/vehicle_analyze.dart
+++ front_end_cop_mate/lib/screens/vehicle_analyze.dart
@@ -1,13 +1,100 @@
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const vehicle_analyze(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
 
 class vehicle_analyze extends StatefulWidget {
+  const vehicle_analyze({super.key, required this.title});
+  final String title;
+
   @override
   State<vehicle_analyze> createState() => _vehicle_analyzeState();
 }
 
 class _vehicle_analyzeState extends State<vehicle_analyze> {
+  late List<Breakages> _chartData;
+  late TooltipBehavior _tooltipBehavior;
+
+  @override
+  void initState() {
+    _chartData = getChartData();
+    _tooltipBehavior = TooltipBehavior(enable: true);
+    super.initState();
+  }
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SfCartesianChart(
+        title: ChartTitle(text: 'Vehicle Data Analysis'),
+        legend: Legend(isVisible: true),
+        tooltipBehavior: _tooltipBehavior,
+        series: <ChartSeries<Breakages, DateTime>>[
+          LineSeries<Breakages, DateTime>(
+              name: 'Breakages',
+              dataSource: _chartData,
+              xValueMapper: (Breakages sales, _) => sales.date,
+              yValueMapper: (Breakages sales, _) => sales.p_value,
+              enableTooltip: true)
+        ],
+        //primaryXAxis: DateTimeAxis(), //DateTimeAxis ->Axis type
+        primaryXAxis: DateTimeAxis(
+            title: AxisTitle(
+                text: 'Date',
+                textStyle: TextStyle(
+                  color: Colors.black,
+                  fontFamily: 'Roboto',
+                  fontSize: 12,
+                  // fontStyle: FontStyle.italic,
+                  // fontWeight: FontWeight.w300
+                ))),
+        // primaryYAxis: NumericAxis(edgeLabelPlacement: EdgeLabelPlacement.shift),
+        primaryYAxis: NumericAxis(
+            title: AxisTitle(
+                text: 'Severity',
+                textStyle: TextStyle(
+                  color: Colors.black,
+                  fontFamily: 'Roboto',
+                  fontSize: 12,
+                  // fontStyle: FontStyle.italic,
+                  // fontWeight: FontWeight.w300
+                ))),
+      ),
+    );
   }
+
+  List<Breakages> getChartData() {
+    final List<Breakages> chartData = [
+      Breakages(DateTime(2022, 1, 1), 6),
+      Breakages(DateTime(2022, 2), 11),
+      Breakages(DateTime(2022, 3, 15), 9),
+      Breakages(DateTime(2022, 4), 14),
+      Breakages(DateTime(2022, 5, 1), 10),
+    ];
+    return chartData;
+  }
+}
+
+class Breakages {
+  Breakages(this.date, this.p_value);
+  final DateTime date;
+  final double p_value;
 }
